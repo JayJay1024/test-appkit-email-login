c37a53919248269df4a00e8d03d9212ded7d6a37
JayJay1024 <lygm1062@gmail.com>
2024-11-22 14:14:45 +0800

    hide appkit-button balance

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <main className="min-h-screen flex flex-col justify-center items-center gap-4">
-      <appkit-button label="Connect Wallet" loadingLabel="Connecting..." balance="show" />
+      <appkit-button label="Connect Wallet" loadingLabel="Connecting..." balance="hide" />
       <appkit-account-button />
       <appkit-connect-button label="Connect Wallet" loadingLabel="Connecting..." />
       <appkit-network-button />
