cde6f50469abc75b826e62a35d9f922fe82caa04
JayJay1024 <lygm1062@gmail.com>
2024-11-22 13:46:51 +0800

    add appkit components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
 export default function App() {
-  return <div className="text-blue-500">Home</div>;
+  return (
+    <main className="min-h-screen flex flex-col justify-center items-center gap-4">
+      <appkit-button label="Connect Wallet" loadingLabel="Connecting..." balance="show" />
+      <appkit-account-button />
+      <appkit-connect-button label="Connect Wallet" loadingLabel="Connecting..." />
+      <appkit-network-button />
+    </main>
+  );
 }
