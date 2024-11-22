c01063f0e05f97f8ab64d3f209d34727ad09bb04
JayJay1024 <lygm1062@gmail.com>
2024-11-22 12:19:29 +0800

    setup reown appkit

---
+++ src/providers/AppKitProvider.tsx
@@ -0,0 +1,49 @@
+import { createAppKit } from "@reown/appkit/react";
+
+import { WagmiProvider } from "wagmi";
+import { AppKitNetwork, arbitrum, mainnet } from "@reown/appkit/networks";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
+
+// 0. Setup queryClient
+const queryClient = new QueryClient();
+
+// 1. Get projectId from https://cloud.reown.com
+const projectId = "fb2ed568967750558b2b61b251c31cc7";
+
+// 2. Create a metadata object - optional
+const metadata = {
+  name: "Test AppKit Email Login",
+  description: "Test AppKit Email Login Example",
+  url: "https://reown.com/appkit", // origin must match your domain & subdomain
+  icons: ["https://assets.reown.com/reown-profile-pic.png"],
+};
+
+// 3. Set the networks
+const networks: [AppKitNetwork, ...AppKitNetwork[]] = [mainnet, arbitrum];
+
+// 4. Create Wagmi Adapter
+const wagmiAdapter = new WagmiAdapter({
+  networks,
+  projectId,
+  ssr: false,
+});
+
+// 5. Create modal
+createAppKit({
+  adapters: [wagmiAdapter],
+  networks,
+  projectId,
+  metadata,
+  features: {
+    analytics: false, // Optional - defaults to your Cloud configuration
+  },
+});
+
+export function AppKitProvider({ children }: React.PropsWithChildren<unknown>) {
+  return (
+    <WagmiProvider config={wagmiAdapter.wagmiConfig}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  );
+}
